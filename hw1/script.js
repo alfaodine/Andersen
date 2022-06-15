b7fefe93363e2ffabcc5d2a52939f1ee3b39c9bc
alfaodine <yatskevych.andrii@gmail.com>
2022-06-15 12:41:13 +0300

    HW 1 task 2

--- hw1/script.js
+++ hw1/script.js
@@ -1,24 +1,3 @@
-function getNumberInOtherSystem() {
-let a = +prompt('Enter first number');
-let b = +prompt('Enter second number');
-    if ((!isNaN(a)) && (!isNaN(b))){
-        let x = a;
-        let arr = [];
-        while (x > 0) {
-            arr.push(x%(b))
-            x = x / b;
-            x = Math.floor(x);
-        }
-        console.log(arr.reverse().join(''));
-    } else {
-        console.log('Некорректный ввод!');
-    }
-}
-
-getNumberInOtherSystem();
-
-//----------- 2 -------------------
-
 function getAnswerTwoNumbers (){
     let a = +prompt('Enter first number');
     if (isNaN(a)) {
@@ -33,4 +12,4 @@ function getAnswerTwoNumbers (){
     console.log(`Ответ: ${a+b}, ${a/b}`)
 }
 
-// getAnswerTwoNumbers();
+getAnswerTwoNumbers();
